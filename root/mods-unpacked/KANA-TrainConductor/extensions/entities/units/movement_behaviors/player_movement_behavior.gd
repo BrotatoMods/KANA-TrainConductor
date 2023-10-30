3bbee013ed3177abe043aa185823faf593390c06
Kai <kai@kana.jetzt>
2023-10-30 11:42:55 +0100

    fix: :bug: fixed 4 way movement not being 4 way

--- root/mods-unpacked/KANA-TrainConductor/extensions/entities/units/movement_behaviors/player_movement_behavior.gd
+++ root/mods-unpacked/KANA-TrainConductor/extensions/entities/units/movement_behaviors/player_movement_behavior.gd
@@ -9,13 +9,13 @@ func get_movement():
 
 	if RunData.effects["kana_move_four_ways"]:
 		if Input.is_action_pressed('ui_right'):
-			movement.x += 1
+			movement = Vector2(1, 0)
 		elif Input.is_action_pressed('ui_left'):
-			movement.x -= 1
+			movement = Vector2(-1, 0)
 		elif Input.is_action_pressed('ui_down'):
-			movement.y += 1
+			movement = Vector2(0, 1)
 		elif Input.is_action_pressed('ui_up'):
-			movement.y -= 1
+			movement = Vector2(0, -1)
 
 	if RunData.effects["kana_cant_stop_moving"] and RunData.effects["kana_move_four_ways"] and movement == Vector2.ZERO:
 		if kana_last_movement == Vector2.ZERO:
