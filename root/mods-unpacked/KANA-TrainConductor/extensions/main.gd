baeb72cad74b91dd4b388aa55ae204056b855f77
Kai <kai@kana.jetzt>
2023-10-30 17:36:45 +0100

    fix: :pencil2: fix spelling

--- root/mods-unpacked/KANA-TrainConductor/extensions/main.gd
+++ root/mods-unpacked/KANA-TrainConductor/extensions/main.gd
@@ -76,21 +76,21 @@ func KANA_draw_debug_points() -> void:
 		new_debug_point.global_position = position
 
 
-func KANA_get_oposite_collision_tile_position(tile_position: Vector2, direction: Vector2) -> Vector2:
+func KANA_get_opposite_collision_tile_position(tile_position: Vector2, direction: Vector2) -> Vector2:
 	var current_zone = ZoneService.get_zone_data(RunData.current_zone).duplicate()
-	var oposite_tile_position: Vector2
+	var opposite_tile_position: Vector2
 	var spawn_offset = 2
 
 	if direction == Vector2(1, 0):
-		oposite_tile_position = Vector2(current_zone.width - spawn_offset, tile_position.y)
+		opposite_tile_position = Vector2(current_zone.width - spawn_offset, tile_position.y)
 	elif direction == Vector2(-1, 0):
-		oposite_tile_position = Vector2(spawn_offset, tile_position.y)
+		opposite_tile_position = Vector2(spawn_offset, tile_position.y)
 	elif direction == Vector2(0, 1):
-		oposite_tile_position = Vector2(tile_position.x, current_zone.height - spawn_offset)
+		opposite_tile_position = Vector2(tile_position.x, current_zone.height - spawn_offset)
 	elif direction == Vector2(0, -1):
-		oposite_tile_position = Vector2(tile_position.x, spawn_offset)
+		opposite_tile_position = Vector2(tile_position.x, spawn_offset)
 
-	var local_position: Vector2 = _tile_map_limits.map_to_world(oposite_tile_position)
+	var local_position: Vector2 = _tile_map_limits.map_to_world(opposite_tile_position)
 	var global_position: Vector2 = _tile_map_limits.to_global(local_position)
 
 	return global_position
@@ -100,9 +100,9 @@ func _KANA_on_player_border_collided(collision: KinematicCollision2D) -> void:
 	var tile_position_player: Vector2 = collision.collider.world_to_map(_player.position)
 	var tile_position_collider: Vector2 = tile_position_player - collision.normal
 
-	var oposite_position := KANA_get_oposite_collision_tile_position(tile_position_collider, collision.normal)
+	var opposite_position := KANA_get_opposite_collision_tile_position(tile_position_collider, collision.normal)
 
-	_player.global_position = oposite_position
+	_player.global_position = opposite_position
 
 
 func _KANA_on_last_position_updated(last_position: Vector2) -> void:
