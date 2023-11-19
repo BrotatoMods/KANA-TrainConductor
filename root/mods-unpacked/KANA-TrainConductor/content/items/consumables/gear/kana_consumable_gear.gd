9ea5143f1e30dfd885d3e066f47aabac8a015f99
Kai <kai@kana.jetzt>
2023-11-19 10:26:14 +0100

    fix: :bug: fixed gear outline and hitbox
    Only setting it in the scene in the editor is not enough. In the exported mod, these values are lost, apparently.

--- root/mods-unpacked/KANA-TrainConductor/content/items/consumables/gear/kana_consumable_gear.gd
+++ root/mods-unpacked/KANA-TrainConductor/content/items/consumables/gear/kana_consumable_gear.gd
@@ -2,7 +2,11 @@ extends Consumable
 
 
 onready var animation_player: AnimationPlayer = $"%AnimationPlayer"
+onready var kana_sprite: Sprite = $Sprite
+onready var kana_collision_shape: CollisionShape2D = $CollisionShape2D
 
 
 func _ready() -> void:
 	animation_player.play("fade_in")
+	kana_sprite.material.set_shader_param("outline_color", Color("ffd929"))
+	kana_collision_shape.shape.extents = Vector2(46, 46)
